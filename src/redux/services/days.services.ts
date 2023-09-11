1c205173faa17bd17db0ce4a368d92bacd27d32f
manojcchoudhary <manojcchoudhary@gmail.com>
2023-09-11 08:01:28 +0530

    fix: async backend updates + instant frontend updates

--- src/redux/services/days.services.ts
+++ src/redux/services/days.services.ts
@@ -4,6 +4,7 @@ import { API_ROUTES_CONSTANTS } from "../../lib/constants/routes.constants";
 
 import { DayModalSchemaType } from "../../lib/schema/day.schema";
 import daysSlice from "../features/days.slice";
+import { RootState } from "../store";
 
 // Define a service using a base URL and expected endpoints
 const daysApi = createApi({
@@ -17,11 +18,21 @@ const daysApi = createApi({
         method: "post",
         data,
       }),
-      async onQueryStarted(_queryParam, { dispatch, queryFulfilled }) {
+      async onQueryStarted(
+        _queryParam,
+        { dispatch, queryFulfilled, getState }
+      ) {
+        const state = getState() as RootState;
+        const oldState =
+          state?.days?.entities?.[_queryParam?.dayId || ""] || null;
+        dispatch(daysSlice.actions.upsertOne(_queryParam));
         try {
-          const data = await queryFulfilled;
-          if (data?.data) dispatch(daysSlice.actions.upsertOne(_queryParam));
-        } catch (err) {}
+          await queryFulfilled;
+        } catch (err) {
+          if (oldState) {
+            dispatch(daysSlice.actions.upsertOne(oldState));
+          }
+        }
       },
       invalidatesTags: (result) =>
         // is result available?
